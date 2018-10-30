39b067481c7e1d63a836b1d2e4897a440becd696
Nemo1710 <karlos1710@gmail.com>
2018-10-30 09:29:49 -0500

    Update models.py

--- integracion/ejemplo/models.py
+++ integracion/ejemplo/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 opciones = (
     ('Temperatura/Humedad', 'Temperatura/Humedad'),
+    ('Temperatura', 'Temperatura'),	
     ('Movimiento', 'Movimiento'),
     ('Detector Voltaje', 'Detector Voltaje'),
     ('Medidor Voltaje', 'Medidor Voltaje'),
